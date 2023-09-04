8982078555a90b69c0c28433b4f0c67bf0a9dd88
Cl1117 <crimsonlun@gmail.com>
2023-09-04 22:54:53 +1000

    Added home button and previous page button into EditMovieFragment.java

--- app/src/main/java/com/example/moviebuddy/EditMovieFragment.java
+++ app/src/main/java/com/example/moviebuddy/EditMovieFragment.java
@@ -1,33 +1,70 @@
 package com.example.moviebuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class EditMovieFragment extends Fragment {
 
-    private EditText idEditText, titleEditText, directorsEditText, castsEditText, releaseDateEditText;
+    // Declare UI elements
+    private EditText editTextMovieId;
+    private EditText editTextMovieTitle;
+    private EditText editTextMovieDirectors;
+    private EditText editTextMovieCasts;
+    private EditText editTextMovieReleaseDate;
+    private Button buttonEditMovie;
+    private Button buttonReturnMain;
+    private Button buttonBackPrevious;
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_movie, container, false);
 
-        idEditText = view.findViewById(R.id.editText_movie_id);
-        titleEditText = view.findViewById(R.id.editText_movie_title);
-        directorsEditText = view.findViewById(R.id.editText_movie_directors);
-        castsEditText = view.findViewById(R.id.editText_movie_casts);
-        releaseDateEditText = view.findViewById(R.id.editText_movie_release_date);
+        // Initialize UI elements
+        editTextMovieId = view.findViewById(R.id.editText_movie_id);
+        editTextMovieTitle = view.findViewById(R.id.editText_movie_title);
+        editTextMovieDirectors = view.findViewById(R.id.editText_movie_directors);
+        editTextMovieCasts = view.findViewById(R.id.editText_movie_casts);
+        editTextMovieReleaseDate = view.findViewById(R.id.editText_movie_release_date);
+        buttonEditMovie = view.findViewById(R.id.button_edit_movie);
+        buttonReturnMain = view.findViewById(R.id.button_return_main_movie);
+        buttonBackPrevious = view.findViewById(R.id.button_back_previous_movie);
 
-        Button editButton = view.findViewById(R.id.button_edit_movie);
-        editButton.setOnClickListener(v -> {
-            // Update the selected movie details in data store (e.g. ArrayList)
-            //...
+        // TODO: Implement the actual editing logic here for the buttonEditMovie
+
+        // Set click listener for the "Return to Main Page" button
+        buttonReturnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the main page (assuming MainActivity is the main page)
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set click listener for the "Back to Previous Page" button
+        buttonBackPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go back to the previous fragment or activity
+                if (getActivity() != null) {
+                    getActivity().onBackPressed();
+                }
+            }
         });
 
         return view;
     }
+
+    // TODO: Add other necessary methods or logic for this fragment
 }
+
+
