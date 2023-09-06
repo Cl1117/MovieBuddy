e4049fd465299144d50d3e8b5cadab787d4e7627
Cl1117 <crimsonlun@gmail.com>
2023-09-07 05:25:47 +1000

    View movie record + edit record. Need to add more fields to edit. Need to add text "Tap tp edit movie" to guide user. Need to delete "Edit a movie record"

--- app/src/main/java/com/example/moviebuddy/EditMovieFragment.java
+++ app/src/main/java/com/example/moviebuddy/EditMovieFragment.java
@@ -5,37 +5,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class EditMovieFragment extends Fragment {
 
-    private Button updateButton;  // "Delete movie" button reference removed
+    private EditText titleEditText, directorEditText; // Add other EditText fields as needed
+    private Button updateButton;
     private Movie selectedMovie;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_movie, container, false);
 
-        // Initialize the "Update movie" button
+        // Initialize the fields
+        titleEditText = view.findViewById(R.id.editText_movie_title);
+        directorEditText = view.findViewById(R.id.editText_movie_director);
         updateButton = view.findViewById(R.id.button_update_movie);
 
         Bundle arguments = getArguments();
         if(arguments != null) {
-            // Get the selected movie passed from previous fragment
             selectedMovie = (Movie) arguments.getSerializable("selectedMovie");
+            if(selectedMovie != null) {
+                titleEditText.setText(selectedMovie.getTitle());
+                directorEditText.setText(selectedMovie.getDirectors());
+                // Populate other fields as needed
+            }
         }
 
-        // Set update button listener
         updateButton.setOnClickListener(v -> {
             if(selectedMovie != null) {
-                // TODO: Update the selected movie details with new values
-            }
+                selectedMovie.setTitle(titleEditText.getText().toString());
+                selectedMovie.setDirectors(directorEditText.getText().toString());
+                // Update other movie details as needed
 
-            // Return to previous fragment
-            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-            transaction.replace(R.id.fragment_container, new ViewMoviesFragment());
-            transaction.commit();
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, new ViewMoviesFragment());
+                transaction.commit();
+            }
         });
 
         return view;
