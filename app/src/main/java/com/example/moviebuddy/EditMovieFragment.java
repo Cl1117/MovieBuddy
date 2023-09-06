cdc1a3f0eb7c861ded795364c8d416568de30558
Cl1117 <crimsonlun@gmail.com>
2023-09-07 03:21:57 +1000

    Edit a movie record, hold to delete works. Fixing edit movie button.

--- app/src/main/java/com/example/moviebuddy/EditMovieFragment.java
+++ app/src/main/java/com/example/moviebuddy/EditMovieFragment.java
@@ -4,54 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class EditMovieFragment extends Fragment {
 
-    private ListView moviesListView;
+    private Button updateButton;  // "Delete movie" button reference removed
+    private Movie selectedMovie;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_edit_movie_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_movie, container, false);
 
-        // Initialize ListView
-        moviesListView = view.findViewById(R.id.movies_list_view);
+        // Initialize the "Update movie" button
+        updateButton = view.findViewById(R.id.button_update_movie);
 
-        // Create an array of movie titles from the movies list
-        String[] movieTitles = new String[DataStore.moviesList.size()];
-        for (int i = 0; i < DataStore.moviesList.size(); i++) {
-            movieTitles[i] = DataStore.moviesList.get(i).getTitle();
+        Bundle arguments = getArguments();
+        if(arguments != null) {
+            // Get the selected movie passed from previous fragment
+            selectedMovie = (Movie) arguments.getSerializable("selectedMovie");
         }
 
-        // Set the adapter for the ListView
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, movieTitles);
-        moviesListView.setAdapter(adapter);
-
-        // Set item click listener for the ListView
-        moviesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Movie selectedMovie = DataStore.moviesList.get(position);
-
-                // Create a new fragment to edit the selected movie details and pass the selected movie to it
-                MovieDetailsEditFragment movieDetailsEditFragment = new MovieDetailsEditFragment();
-                Bundle bundle = new Bundle();
-                bundle.putSerializable("selectedMovie", selectedMovie);
-                movieDetailsEditFragment.setArguments(bundle);
-
-                // Replace the current fragment with the new one
-                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragment_container, movieDetailsEditFragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
+        // Set update button listener
+        updateButton.setOnClickListener(v -> {
+            if(selectedMovie != null) {
+                // TODO: Update the selected movie details with new values
             }
+
+            // Return to previous fragment
+            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, new ViewMoviesFragment());
+            transaction.commit();
         });
 
         return view;
     }
 }
-
--- app/src/main/java/com/example/moviebuddy/MovieDetailsEditFragment.java
+++ app/src/main/java/com/example/moviebuddy/MovieDetailsEditFragment.java
@@ -1,27 +1,53 @@
 package com.example.moviebuddy;
 
-import android.os.Bundle;
+// Import necessary packages at the top
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
+import com.example.moviebuddy.R;
+
 
 public class MovieDetailsEditFragment extends Fragment {
+
+    private ListView moviesListView;
+    private ArrayAdapter<String> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_movie_details_edit, container, false);
 
+        moviesListView = view.findViewById(R.id.lv_movies_list);
 
-        // Back to previous page button
-        Button backButton = view.findViewById(R.id.btn_back_edit);
-        backButton.setOnClickListener(v -> {
-            // Go back to the previous fragment
-            if (getFragmentManager() != null) {
-                getFragmentManager().popBackStack();
+        // Dummy data for movies. Replace with actual data source.
+        String[] movies = {"Movie 1", "Movie 2", "Movie 3"};
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, movies);
+        moviesListView.setAdapter(adapter);
+
+        // Handle click event to edit movie
+        moviesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedMovie = adapter.getItem(position);
+                // Start edit movie activity or fragment
+                // TODO: Implement edit movie logic here
+            }
+        });
+
+        // Handle long click event to delete movie
+        moviesListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedMovie = adapter.getItem(position);
+                // TODO: Show confirmation dialog and handle delete logic
+                return true; // return true to indicate that the event is consumed
             }
         });
 
